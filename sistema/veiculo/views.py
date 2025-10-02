ae87fb3db1991d4d1a8f751922815be6a6c9e2aa
MarcosFrMelo <143053350+MarcosFrMelo@users.noreply.github.com>
2025-10-02 20:58:46 -0300

    Add vehicle photo upload functionality and display; update templates and views

--- sistema/veiculo/views.py
+++ sistema/veiculo/views.py
@@ -5,6 +5,9 @@ from veiculo.models import Veiculo
 from veiculo.forms import FormularioVeiculo
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.http import HttpResponse, FileResponse, Http404
+from django.views.generic import View
+from django.core.exceptions import ObjectDoesNotExist
 
 class ListarVeiculos(LoginRequiredMixin, ListView):
     """
@@ -23,3 +26,16 @@ class CriarVeiculos(LoginRequiredMixin, CreateView):
     form_class = FormularioVeiculo
     template_name = 'veiculo/novo.html'
     success_url = reverse_lazy('listar-veiculos')
+
+class FotoVeiculo(View):
+    """
+    View para exibir a foto de um veículo.
+    """
+    def get(self, request, arquivo):
+        try:
+            veiculo = Veiculo.objects.get(foto='veiculo/fotos/{}'.format(arquivo))
+            return FileResponse(veiculo.foto)
+        except ObjectDoesNotExist:
+            raise Http404("Foto do Veículo não encontrado")
+        except Exception as e:
+            raise e
